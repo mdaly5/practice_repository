c064b20cc425137a2fd02f04176d0573bcb47ebb
mdaly5 <68384175+mdaly5@users.noreply.github.com>
2023-04-17 13:12:05 -0500

    Add files via upload

---
+++ pygame practice.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+vec = pygame.math.Vector2
+
+HEIGHT = 450
+WIDTH = 400
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+
+FramePerSec = pygame.time.Clock()
+
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((128,255,40))
+        self.rect = self.surf.get_rect(center = (10, 420))
+ 
+class platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, 20))
+        self.surf.fill((255,0,0))
+        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
+ 
+PT1 = platform()
+P1 = Player()
